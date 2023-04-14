ba19206dfc1682e1ea52a922f4be94132fce06db
Toba Joseph <tobajoseph@outlook.com>
2023-04-15 00:33:02 +0100

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function(eleventyConfig){
+   eleventyConfig.addPassthroughCopy("./src/css");
+   eleventyConfig.addPassthroughCopy("./src/fonts");
+   eleventyConfig.addPassthroughCopy("./src/images");
+   eleventyConfig.addPassthroughCopy("./src/js");
+   eleventyConfig.addPassthroughCopy("./src/scss");
+   eleventyConfig.addPassthroughCopy("./src/admin");
+   eleventyConfig.addPassthroughCopy("./src/_redirects");
+   eleventyConfig.addPassthroughCopy({ "./src/favicon.ico": "/favicon.ico"});
+   eleventyConfig.addPassthroughCopy({ "./src/robots.txt": "/robots.txt"});
+   return{
+      passthroughFileCopy: true,
+      markdownTemplateEngine:"njk",
+      templateFormats:["html", "njk", "md"],
+      dir:{
+         input: "src",
+         output: "_site",
+         includes: "_includes",
+         layouts: "_layouts"
+      }
+   };
+};

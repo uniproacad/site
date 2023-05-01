3a2d20899a099383d4ed9c3438759847a9c5dd8c
Toba Joseph <tobajoseph@outlook.com>
2023-05-01 19:07:35 +0100

    updated the submit form

--- src/js/submit-form.js
+++ src/js/submit-form.js
@@ -1,24 +1,36 @@
-  window.onload = function () {
-    const form = document.getElementById("upma-form");
-    const submitButton = form.querySelector('button[type="submit"]');
+window.onload = function () {
+  const form = document.querySelector('#upma-form');
+  const submitButton = form.querySelector('button[type="submit"]');
 
-    form.addEventListener("submit", function (e) {
-      e.preventDefault();
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    if (submitButton) {
       submitButton.disabled = true; // disable the submit button
-
-      const data = new FormData(form);
-      const action = e.target.action;
-      fetch(action, {
-        method: "POST",
-        body: data,
-      })
-        .then(() => {
-          alert("Message sent successfully. Thank you!");
-          form.reset(); // reset the form after successful submission
-        })
-        .catch((error) => console.error(error)) // catch and log any errors
-        .finally(() => {
+    }
+    fetch(form.action, {
+      method: form.method,
+      body: new FormData(form)
+    })
+    .then(response => {
+      if (response.ok) {
+        // Handle the successful submission here.
+        alert("Message sent successfully. Thank you!");
+        form.reset(); // reset the form after successful submission
+      } else {
+        // Handle the submission error here.
+        alert("Sorry, could not process your request at this time. Please, try again later.");
+      }
+    })
+    .catch(error => {
+      // Handle any network or other errors here.
+      //console.error(error);
+      alert("An error just occured. Please, check your connection and try again.");
+    })
+    .finally(() => {
+        if (submitButton) {
           submitButton.disabled = false; // re-enable the submit button
-        });
+        }
     });
-  };
+  });
+
+};
